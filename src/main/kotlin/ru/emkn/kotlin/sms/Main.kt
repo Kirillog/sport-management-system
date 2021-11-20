2f0225bd2b7a64861cfa9f129005b6ac96cebad2
khaser <a-horohorin@mail.ru>
2021-11-20 12:25:12 +0300

    Included argParser library

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,5 +1,17 @@
 package ru.emkn.kotlin.sms
 
-fun main(args: Array<String>) {
-    TODO()
+//https://github.com/xenomachina/kotlin-argparser
+import com.xenomachina.argparser.*
+
+class MyArgs(parser: ArgParser) {
+    val verbose by parser.flagging("-v", "--verbose", help="enable verbose mode")
+
+    val name by parser.storing("-n", "--name", help="user name").default<String>("kek")
+
+    val count by parser.storing("-c", "--count", help="test counter") { toIntOrNull() ?: println("null here") }
+}
+
+fun main(args: Array<String>) = mainBody {
+    val parsedArgs = ArgParser(args).parseInto(::MyArgs)
+    println("Hello ${parsedArgs.name}!")
 }
