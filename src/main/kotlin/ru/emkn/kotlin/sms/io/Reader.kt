865ee215559aa7b0b756eb8a1f24910b683689be
Kirillog <kirill.mitkin.03@mail.ru>
2021-11-24 21:01:28 +0300

    added read functions

--- src/main/kotlin/ru/emkn/kotlin/sms/io/Reader.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/io/Reader.kt
@@ -2,9 +2,7 @@ package ru.emkn.kotlin.sms.io
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import mu.KotlinLogging
-import ru.emkn.kotlin.sms.objects.Course
-import ru.emkn.kotlin.sms.objects.Event
-import ru.emkn.kotlin.sms.objects.Team
+import ru.emkn.kotlin.sms.objects.*
 import java.io.File
 
 abstract class Reader(protected val file: File) {
@@ -33,4 +31,38 @@ fun formTeamsList(): List<Team> {
             CSVReader(file, this).team()
         }
     }.toList().filterNotNull()
+}
+
+fun formGroupsList(teams: List<Team>): List<Group> {
+    val reader = csvReader()
+    val file = File(competitionPath + "input/classes.csv")
+    val map = reader.open(file) {
+        CSVReader(file, this).groupsToCourses()
+    } ?: throw IllegalArgumentException("Cannot read ${file.name}, program was terminated")
+    val courses = formCoursesList().associateBy { course -> course.name }
+    return teams.flatMap(Team::members)
+        .groupBy(Participant::group)
+        .map { group ->
+            Group(
+                group.key, courses[map[group.key]]
+                    ?: throw IllegalArgumentException("${group.key} doesn't have appropriate course"),
+                group.value
+            )
+        }
+}
+
+fun formCoursesList(): List<Course> {
+    val reader = csvReader()
+    val file = File(competitionPath + "input/courses.csv")
+    return reader.open(file) {
+        CSVReader(file, this).courses()
+    } ?: throw IllegalArgumentException("Cannot read ${file.name}, program was terminated")
+}
+
+fun formEventsList(): List<Event> {
+    val reader = csvReader()
+    val file = File(competitionPath + "input/event.csv")
+    return reader.open(file) {
+        CSVReader(file, this).events()
+    } ?: throw IllegalArgumentException("Cannot read ${file.name}, program was terminated")
 }
