b12c3c67a460ffed2f478867955d47f56bcf5c5f
khaser <a-horohorin@mail.ru>
2021-11-25 21:03:22 +0300

    simpleToss implemented

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -12,23 +12,19 @@ import ru.emkn.kotlin.sms.io.formEvent
 import ru.emkn.kotlin.sms.io.formGroupsList
 import ru.emkn.kotlin.sms.io.formTeamsList
 import ru.emkn.kotlin.sms.objects.Competition
+import ru.emkn.kotlin.sms.objects.Group
 import java.nio.file.Path
+import java.time.LocalTime
 import kotlin.io.path.Path
 
 private val logger = KotlinLogging.logger {}
 
+
 fun tossTarget(competition: Competition) {
-//    TODO()
+    competition.simpleToss(LocalTime.NOON, 5)
     val writer = Writer(competition.path.resolve("protocols/toss.csv").toFile(), FileType.CSV)
     writer.addAll(competition.groups)
     writer.write()
-}
-
-fun makeCompetition(path: Path): Competition {
-    val event = formEvent(path)
-    val teams = formTeamsList(path)
-    val groups = formGroupsList(teams, path)
-    return Competition(event, path, teams, groups)
 }
 
 fun main(args: Array<String>): Unit = mainBody {
@@ -37,11 +33,7 @@ fun main(args: Array<String>): Unit = mainBody {
     val parsedArgs = ArgParser(args).parseInto(::ArgumentsFormat)
     val competitionPath = Path(parsedArgs.competitionsRoot).resolve(parsedArgs.competitionName)
 
-    val competition = try {
-        makeCompetition(competitionPath)
-    } catch (error: Exception) {
-        TODO()
-    }
+    val competition = Competition(competitionPath)
     logger.info { "Competition files read success" }
 
     when (parsedArgs.target) {
--- src/main/kotlin/ru/emkn/kotlin/sms/objects/Participant.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/objects/Participant.kt
@@ -3,6 +3,7 @@ package ru.emkn.kotlin.sms.objects
 import kotlinx.datetime.LocalDateTime
 import ru.emkn.kotlin.sms.io.Readable
 import ru.emkn.kotlin.sms.io.SingleLineWritable
+import java.time.LocalTime
 
 class Participant(
     val name: String,
@@ -12,9 +13,9 @@ class Participant(
     val team: String,
     val grade: String? = null
 ) : Readable, SingleLineWritable {
-    private val id: Int? = null
-    private val startTime: LocalDateTime? = null
-    private val finishTime: LocalDateTime? = null
+    var id: Int? = null
+    var startTime: LocalTime? = null
+    var finishTime: LocalTime? = null
     override fun toLine(): List<String> {
         val result = mutableListOf(name, surname, birthdayYear.toString(), group, team)
         if (grade != null)
