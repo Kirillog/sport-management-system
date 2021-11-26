75bffe5e241a4fc1a2b4dfc96ba231b6c1bdc8e0
khaser <a-horohorin@mail.ru>
2021-11-26 21:58:43 +0300

    completed tests for Writer

--- src/test/kotlin/WriterTest.kt
+++ src/test/kotlin/WriterTest.kt
@@ -3,7 +3,10 @@ import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import org.junit.Test
 import ru.emkn.kotlin.sms.FileType
 import ru.emkn.kotlin.sms.io.Writer
+import ru.emkn.kotlin.sms.objects.Course
+import ru.emkn.kotlin.sms.objects.Group
 import ru.emkn.kotlin.sms.objects.Participant
+import ru.emkn.kotlin.sms.objects.Team
 import java.io.File
 import kotlin.test.assertEquals
 
@@ -13,10 +16,24 @@ class WriterTest {
     private val file = File(fileName).also(File::deleteOnExit)
     private val writer = Writer(file, FileType.CSV)
 
+    private val participant = listOf(
+        Participant("Vasya", "Pupkin", 1998, "Human", "Team A", "I"),
+        Participant("Petya", "Loopkin", 2189, "Human", "Team B"),
+        Participant("Picathu", "Yellow", 201, "Pokemon", "Team A", "IV"),
+        Participant("Squirtle", "Blue", 211, "Pokemon", "Team B")
+    )
+
+    private val participantDump = listOf(
+        listOf("Vasya", "Pupkin", "1998", "Human", "Team A", "I"),
+        listOf("Petya", "Loopkin", "2189", "Human", "Team B", ""),
+        listOf("Picathu", "Yellow", "201", "Pokemon", "Team A", "IV"),
+        listOf("Squirtle", "Blue", "211", "Pokemon", "Team B", "")
+    )
+
     @Test
     fun testWriteApplicant() {
-        val appA = Participant("Vasya", "Pupkin", 1998, "30M", "Byaka", "I")
-        val appB = Participant("Petya", "Loopkin", 2189, "18M", "Buka")
+        val appA = participant[0]
+        val appB = participant[1]
 
         writer.add(appA)
         writer.add(appB)
@@ -24,10 +41,10 @@ class WriterTest {
         writer.write()
 
         val correct = listOf(
-            listOf("Vasya", "Pupkin", "1998", "30M", "Byaka", "I"),
-            listOf("Petya", "Loopkin", "2189", "18M", "Buka", ""),
-            listOf("Petya", "Loopkin", "2189", "18M", "Buka", ""),
-            listOf("Vasya", "Pupkin", "1998", "30M", "Byaka", "I")
+            participantDump[0],
+            participantDump[1],
+            participantDump[1],
+            participantDump[0]
         )
         val csvData = csvReader().readAll(file)
         assertEquals(correct, csvData)
@@ -44,6 +61,52 @@ class WriterTest {
             listOf("Hello", ""),
             listOf("CSV", ""),
             listOf("string", "input")
+        )
+        val csvData = csvReader().readAll(file)
+        assertEquals(correct, csvData)
+    }
+
+    @Test
+    fun testWriteTeam() {
+        val teamA = Team(name = "Team A",  listOf(participant[0], participant[2]))
+        val teamB = Team(name = "Team B", listOf(participant[1], participant[3]))
+
+        writer.add(teamA)
+        writer.addAll(listOf(teamB))
+        writer.write()
+
+        val correct = listOf(
+            listOf("Team A", "", "", "", "", ""),
+            participantDump[0],
+            participantDump[2],
+            listOf("Team B", "", "", "", "", ""),
+            participantDump[1],
+            participantDump[3]
+        )
+        val csvData = csvReader().readAll(file)
+        assertEquals(correct, csvData)
+    }
+
+    @Test
+    fun testWriteGroup() {
+        val groupA = Group(name = "Human", course = Course("Course for 'Human'", listOf()),
+            listOf(participant[0], participant[1])
+        )
+        val groupB = Group(name = "Pokemon", course = Course("Course for 'Pokemon'", listOf()),
+            listOf(participant[2], participant[3])
+        )
+
+        writer.add(groupA)
+        writer.addAll(listOf(groupB))
+        writer.write()
+
+        val correct = listOf(
+            listOf("Human", "", "", "", "", ""),
+            participantDump[0],
+            participantDump[1],
+            listOf("Pokemon", "", "", "", "", ""),
+            participantDump[2],
+            participantDump[3]
         )
         val csvData = csvReader().readAll(file)
         assertEquals(correct, csvData)
