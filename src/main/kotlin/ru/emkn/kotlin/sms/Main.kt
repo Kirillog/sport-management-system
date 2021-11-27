91c285e3109d2fb728f98eff49630e16885686fe
Kirillog <kirill.mitkin.03@mail.ru>
2021-11-27 22:22:37 +0300

    added catching of errors

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -19,7 +19,7 @@ fun tossTarget(competition: Competition) {
     competition.simpleToss(LocalTime.NOON, 5)
     val writer = Writer(competition.path.resolve("protocols/toss.csv").toFile(), FileType.CSV)
 
-    writer.add(listOf("Номер", "Имя", "Фамилия", "Г.р.", "Команда", "Разряд", "Время старта"))
+    writer.add(listOf("Номер", "Имя", "Фамилия", "Г.р.", "Команда", "Разр.", "Время старта"))
     writer.addAll(competition.groups)
     writer.write()
 }
@@ -29,14 +29,19 @@ fun main(args: Array<String>): Unit = mainBody {
     logger.info { "Program started" }
     val parsedArgs = ArgParser(args).parseInto(::ArgumentsFormat)
     val competitionPath = Path(parsedArgs.competitionsRoot).resolve(parsedArgs.competitionName)
+    try {
+        val competition = Competition(competitionPath)
+        logger.info { "Competition files read success" }
 
-    val competition = Competition(competitionPath)
-    logger.info { "Competition files read success" }
-
-    when (parsedArgs.target) {
-        Target.TOSS -> tossTarget(competition)
-        Target.PERSONAL_RESULT -> TODO()
-        Target.TEAM_RESULT -> TODO()
+        when (parsedArgs.target) {
+            Target.TOSS -> tossTarget(competition)
+            Target.PERSONAL_RESULT -> TODO()
+            Target.TEAM_RESULT -> TODO()
+        }
+        logger.info { "Program successfully finished" }
     }
-    logger.info { "Program successfully finished" }
+    catch (error : Exception) {
+        logger.info {"Wow, that's a big surprise, program was fault"}
+        logger.error { error.message }
+    }
 }
--- src/main/kotlin/ru/emkn/kotlin/sms/objects/Competition.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/objects/Competition.kt
@@ -1,20 +1,19 @@
 package ru.emkn.kotlin.sms.objects
 
+import mu.KotlinLogging
 import ru.emkn.kotlin.sms.io.formEvent
 import ru.emkn.kotlin.sms.io.formGroupsList
 import ru.emkn.kotlin.sms.io.formTeamsList
 import java.nio.file.Path
 import java.time.LocalTime
 
+private val logger = KotlinLogging.logger {}
+
 fun makeCompetition(path: Path): Competition {
-    return try {
-        val event = formEvent(path)
-        val teams = formTeamsList(path)
-        val groups = formGroupsList(teams, path)
-        Competition(event, path, teams, groups)
-    } catch (error: Exception) {
-        TODO()
-    }
+    val event = formEvent(path)
+    val teams = formTeamsList(path)
+    val groups = formGroupsList(teams, path)
+    return Competition(event, path, teams, groups)
 }
 
 data class Competition(val event: Event, val path: Path, val teams: List<Team>, val groups: List<Group>) {
