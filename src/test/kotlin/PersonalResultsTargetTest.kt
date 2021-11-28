543f0541f0095991b70997c344ef4ff72674000c
seva-vaskin <vaskin.2003@gmail.com>
2021-11-28 20:26:58 +0300

    added competition-2 test files

---
+++ src/test/kotlin/PersonalResultsTargetTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import ru.emkn.kotlin.sms.targets.personalResultsTarget
+import kotlin.io.path.Path
+import kotlin.test.assertEquals
+
+internal class PersonalResultsTargetTest {
+
+    private val competition2Path = Path("src/test/resources/competition-2")
+
+    @Test
+    fun testPersonalResultsTarget() {
+        personalResultsTarget(competition2Path)
+        val expected = competition2Path.resolve("protocols/expectedResults.csv").toFile().useLines { it.toList() }
+        val actual = competition2Path.resolve("protocols/results.csv").toFile().useLines { it.toList() }
+        assertEquals(expected, actual)
+    }
+}
