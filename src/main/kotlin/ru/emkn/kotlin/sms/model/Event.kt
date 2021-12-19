0f1821bc4f3c1da14a7759e353c56913f1400020
khaser <a-horohorin@mail.ru>
2021-12-19 22:19:43 +0300

    saving event

--- src/main/kotlin/ru/emkn/kotlin/sms/model/Event.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/model/Event.kt
@@ -1,8 +1,23 @@
 package ru.emkn.kotlin.sms.model
 
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.Table.Dual.varchar
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.insert
+import ru.emkn.kotlin.sms.MAX_TEXT_FIELD_SIZE
 import ru.emkn.kotlin.sms.io.SingleLineWritable
+import ru.emkn.kotlin.sms.model.EventTable.date
 import java.time.LocalDate
+import org.jetbrains.exposed.sql.javatime.date
 import java.time.format.DateTimeFormatter
+
+
+object EventTable : Table("event") {
+    val name: Column<String> = varchar("name", MAX_TEXT_FIELD_SIZE)
+    val date: Column<LocalDate> = date("date")
+}
 
 /**
  * Class for storing metadata about the competition.
@@ -17,5 +32,13 @@ data class Event(var name: String, var date: LocalDate) : SingleLineWritable {
     fun change(name: String, date: LocalDate) {
         this.name = name
         this.date = date
+    }
+
+    init {
+        EventTable.deleteAll()
+        EventTable.insert {
+            it[this.name] = this@Event.name
+            it[this.date] = this@Event.date
+        }
     }
 }
